e9952f3d193f33da40037d9d3c50c6a05deccef3
SukHyunil <sukhyunil@naver.com>
2023-08-14 22:12:30 +0900

    test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "github.io 블로그 첫 포스팅입니다",
+        "excerpt":"🔔포스팅 공지  개인 공부 기록용 블로그 입니다.  잘못된 부분이 있을 시 메일이나 댓글로 지적해주시면 감사드리겠습니다 :)  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/post/",
+        "teaser": null
+      },{
+        "title": "Redis 실무에 사용하면서 정리한 내용",
+        "excerpt":"Redis란? 대규모 서비스를 운영하기 위해선 데이터를 안전하게 그리고 빠르게 저장하고 불러오는 기술이 필요합니다. 이런 기술을 위해 Redis 가 존재하고 누군가는 Redis 를 캐싱 솔루션이라고 부르기도 하고 누군가는 NoSQL 의 Key-Value 저장소라고 부르기도 합니다. Redis의 주요 특성 Redis 의 주요 특징 |항목|내용| |-|—–| |Key-Value 스토어|Key-Value 를 저장할 수 있는 스토리지를 지원한다.|...","categories": ["DataBase"],
+        "tags": ["Redis","NoSQL","Key-Value 저장소"],
+        "url": "/database/post3/",
+        "teaser": null
+      }]
